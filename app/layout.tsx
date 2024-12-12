db6e3c8f7426d8a67566e4c95f6ed316102fcd6d
Youssef-KhaledMo <youssef.khaled.25@h-eng.helwan.edu.eg>
2024-12-12 22:56:02 +0200

    initialize project with Next.js, ESLint, PostCSS, Tailwind CSS, and basic components

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import "./globals.css";
+import { Metadata } from "next";
+import { Toaster } from "sonner";
+import { ThemeProvider } from "next-themes";
+import { Inter } from "next/font/google";
+
+const geist = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://ai-sdk-preview-pdf-support.vercel.app"),
+  title: "PDF Support Preview",
+  description: "Experimental preview of PDF support with the AI SDK",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning className={`${geist.className}`}>
+      <body>
+        <ThemeProvider attribute="class" enableSystem forcedTheme="dark">
+          <Toaster position="top-center" richColors />
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
